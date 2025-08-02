bd4a34979b320212ee32370555b6bee478ea352e
zedai00 <33319711+Zedai00@users.noreply.github.com>
2025-08-02 11:30:44 +0530

    Add Open Message Details

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,11 +4,13 @@ const indexRouter = Router();
 
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Zed",
     added: new Date(),
   },
   {
+    id: 2,
     text: "Hello World!",
     user: "AI",
     added: new Date(),
@@ -25,11 +27,21 @@ indexRouter.get("/new", (req, res) => {
 
 indexRouter.post("/new", (req, res) => {
   messages.push({
+    id: messages.length + 1,
     text: req.body.message,
     user: req.body.user,
     added: new Date(),
   });
   res.redirect("/");
 });
+
+indexRouter.get("/message/:id", (req, res) => {
+  const message = messages.find(msg => msg.id === parseInt(req.params.id))
+  if (!message) {
+    return res.status(404).send("Message not found")
+  }
+
+  res.render("detail", { title: "Message Detail", message })
+})
 
 module.exports = indexRouter;
