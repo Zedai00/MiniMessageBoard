0e1eeb55b3e168716e26627241b93f2a2ce5d2a7
zedai00 <33319711+Zedai00@users.noreply.github.com>
2025-08-08 16:16:22 +0530

    Add Dark Pallete, Edit and Delete functions

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -44,4 +44,34 @@ indexRouter.get("/message/:id", (req, res) => {
   res.render("detail", { title: "Message Detail", message })
 })
 
+indexRouter.post("/message/:id/delete", (req, res) => {
+  const id = parseInt(req.params.id)
+  const index = messages.findIndex(msg => msg.id === id)
+  if (index === -1) {
+    return res.status(404).send("Message not found")
+  }
+
+  messages.splice(index, 1)
+  res.redirect("/")
+})
+
+indexRouter.get("/message/:id/edit", (req, res) => {
+  const message = messages.find(msg => msg.id === parseInt(req.params.id));
+  if (!message) return res.status(404).send("Message not found");
+  res.render("edit", { title: "Edit Message", message });
+});
+
+indexRouter.post("/message/:id/edit", (req, res) => {
+  const id = parseInt(req.params.id);
+  const message = messages.find(msg => msg.id === id);
+  if (!message) return res.status(404).send("Message not found");
+
+  message.user = req.body.user;
+  message.text = req.body.message;
+  message.added = new Date();
+
+  res.redirect(`/message/${id}`);
+});
+
+
 module.exports = indexRouter;
